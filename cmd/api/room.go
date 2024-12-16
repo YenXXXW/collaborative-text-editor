f4865102c9821a9e7ce44834e592ca4544a4b142
Wai <wai.yan@impact-h.co.jp>
2024-12-16 22:36:33 +0630

    implement the websokcet for room

--- cmd/api/room.go
+++ cmd/api/room.go
@@ -1,15 +1,58 @@
 package main
 
 import (
-	"github.com/gorilla/websocket"
-	"github.com/yenxxxw/collaborative-text-editor/internal/store"
+	"errors"
 	"log"
 	"net/http"
+	"sync"
+
+	"github.com/gorilla/websocket"
+	"github.com/yenxxxw/collaborative-text-editor/internal/store"
 )
 
 type CreateRoomPayload struct {
 	UsersInRoom []int64 `json:"users_in_room"`
 	CeatorId    int64   `json:"creator_id"`
+}
+
+type Client struct {
+	Conn   *websocket.Conn
+	RoomId string
+	Send   chan []byte
+}
+
+type RoomManager struct {
+	Rooms map[string]map[*Client]bool
+	Mutex sync.Mutex
+}
+
+func (rm *RoomManager) JoinRoom(roomId string, client *Client) {
+	rm.Mutex.Lock()
+	defer rm.Mutex.Unlock()
+
+	if rm.Rooms[roomId] == nil {
+		rm.Rooms[roomId] = make(map[*Client]bool)
+	}
+
+	rm.Rooms[roomId][client] = true
+}
+
+func (rm *RoomManager) LeaveRoom(roomId string, client *Client) {
+	rm.Mutex.Lock()
+	defer rm.Mutex.Unlock()
+
+	if clients, ok := rm.Rooms[roomId]; ok {
+		delete(clients, client)
+		if len(clients) == 0 {
+			delete(rm.Rooms, roomId)
+		}
+	}
+}
+
+func NewRoomManager() *RoomManager {
+	return &RoomManager{
+		Rooms: make(map[string]map[*Client]bool),
+	}
 }
 
 var upgrader = websocket.Upgrader{
@@ -56,10 +99,25 @@ func (app *application) createRoomHandler(w http.ResponseWriter, r *http.Request
 	}
 }
 
-func (app *application) joinRoomHandler(w http.ResponseWriter, r *http.Request) error {
-	if conn, err := upgrader.Upgrade(w, r, nil); err != nil {
+func (app *application) joinRoomHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
 		app.internalServerError(w, r, err)
 		log.Println(err)
 		return
 	}
+
+	roomID := r.URL.Query().Get("roomID")
+	if roomID == "" {
+		err := errors.New("Room Id is required")
+		app.internalServerError(w, r, err)
+		conn.Close()
+	}
+
+	client := &Client{
+		Conn:   conn,
+		RoomId: roomID,
+		Send:   make(chan []byte),
+	}
+
 }
