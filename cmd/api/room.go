76258055df0df0892ac3a22ce6c2be4ba5a10ef8
Wai <wai.yan@impact-h.co.jp>
2025-05-24 16:21:55 +0630

    tesing the pong on the hosted server

--- cmd/api/room.go
+++ cmd/api/room.go
@@ -388,6 +388,7 @@ func handleClientReads(client *Client) {
 	client.Conn.SetReadLimit(maxMessageSize)
 	client.Conn.SetReadDeadline(time.Now().Add(pongWait))
 	client.Conn.SetPongHandler(func(string) error {
+		fmt.Println("the pong is received")
 		client.Conn.SetReadDeadline(time.Now().Add(pongWait))
 		return nil
 	})
